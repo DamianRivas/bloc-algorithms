9c0f92fbb8de32ae2cb6610f117a801db841726d
damianrivas <drivas1993@gmail.com>
2018-07-22 20:08:31 -0700

    Intro to Algorithms solution

--- intro_to_algorithms/fibonacci_iterative.rb
+++ intro_to_algorithms/fibonacci_iterative.rb
@@ -1,10 +1,11 @@
 def fib(n)
-  return 1 if n == 0
+  return 0 if n == 0
+  return 1 if n == 1
 
   fib_0 = 0
   fib_1 = 1
 
-  n.times do |i|
+  0.upto(n-2) do
     temp = fib_0
     fib_0 = fib_1
     fib_1 += temp
