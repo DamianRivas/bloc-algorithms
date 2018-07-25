9dfcecc0163f225cc5009c952eaff500d5a83f37
damianrivas <drivas1993@gmail.com>
2018-07-24 21:57:40 -0700

    Benchmark each algorithm against each other

---
+++ sorting/benchmark.rb
@@ -0,0 +1,15 @@
+require "benchmark"
+
+require_relative "./quick_sort.rb"
+require_relative "./bucket_sort.rb"
+require_relative "./node.rb"
+require_relative "./min_binary_heap.rb"
+require_relative "./heap_sort.rb"
+
+collection = Array.new(50) { rand(1..1000) }
+
+Benchmark.bmbm do |x|
+  x.report("quick:") { quick_sort(collection) }
+  x.report("bucket:") { bucket_sort(collection) }
+  x.report("heap:") { heap_sort(collection) }
+end
