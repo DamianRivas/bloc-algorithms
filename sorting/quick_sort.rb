75d418972d4455775c4f0f5751161def3f9b49b5
damianrivas <drivas1993@gmail.com>
2018-07-24 21:06:03 -0700

    Modify quick sort

--- sorting/quick_sort.rb
+++ sorting/quick_sort.rb
@@ -1,6 +1,20 @@
 def quick_sort(collection)
   return collection if collection.size <= 1
   pivot = collection.length - 1
+
+  partition(collection, pivot)
+
+  # Split array into two sub-arrays on either side of pivot and sort them
+  sub_collection_1 = collection[0...pivot]
+  sub_collection_2 = collection[pivot+1...collection.length]
+  p sub_collection_2 if sub_collection_2 == [10, 8, 12]
+  collection = quick_sort(sub_collection_1).push(collection[pivot]).concat(quick_sort(sub_collection_2))
+
+  collection
+end
+
+# Move items that are greater than the pivot directly to the right of the pivot
+def partition(collection, pivot)
   index = 0
 
   while index < pivot
@@ -13,14 +27,6 @@ def quick_sort(collection)
     end
     index += 1
   end
-
-  # Split array into two sub-arrays on either side of pivot and sort them
-  sub_collection_1 = collection[0...pivot]
-  sub_collection_2 = collection[pivot+1...collection.length]
-  p sub_collection_2 if sub_collection_2 == [10, 8, 12]
-  collection = quick_sort(sub_collection_1).push(collection[pivot]).concat(quick_sort(sub_collection_2))
-
-  collection
 end
 
 # Starting from `index`, shift every element in `collection` to the left one time until the pivot is reached.
