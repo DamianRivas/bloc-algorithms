b697f0780240316268ff0f1a15026aae5288286a
damianrivas <drivas1993@gmail.com>
2018-08-07 19:15:23 -0700

    Minor edit

--- travelling_salesman/city.rb
+++ travelling_salesman/city.rb
@@ -7,7 +7,7 @@ class City
     @visited = visited
     @name = name
     @neighbors = []
-    # @neighbors is an array of hashes, where each hash contains a City node, ['city'] as well as
-    # the distance from the current City, ['distance']
+    # @neighbors is an array of hashes, where each hash contains a City node, ['city'], as well as
+    # the distance from the current City, ['distance'].
   end
 end
